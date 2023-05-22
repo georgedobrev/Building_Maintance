00a7ba54dd9eb4e0bf9abb4c2fb0789f73c0a5de
martinbardarov-exe <martin.bardarov@blankfactor.com>
2023-05-22 11:25:09 +0300

    Database and initialization

---
+++ src/test/java/com/blankfactor/building/maintenance/BuildingMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blankfactor.building.maintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
