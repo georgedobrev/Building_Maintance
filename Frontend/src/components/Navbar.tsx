3861a9b29f871d603331ffdf14118f666a6060d2
AntonioNik <antonio.nikolov@blankfactor.com>
2023-06-01 13:49:21 +0300

    F5 Create-navigation-bar

---
+++ Frontend/src/components/Navbar.tsx
@@ -0,0 +1,254 @@
+import { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
+import { styled, useTheme } from "@mui/system";
+
+interface NavbarProps {
+  currentUser: boolean;
+  manager: boolean;
+  serviceCompanyAdmin: boolean;
+  serviceCompany: boolean;
+}
+
+const StyledLink = styled(Link)(({ theme }) => ({
+  textDecoration: "none",
+  color:
+    theme.palette.mode === "dark"
+      ? theme.palette.primary.main
+      : theme.palette.secondary.main,
+}));
+
+const StyledDropdownLink = styled(Link)(({ theme }) => ({
+  textDecoration: "none",
+  color:
+    theme.palette.mode === "dark"
+      ? theme.palette.primary.main
+      : theme.palette.primary.main,
+}));
+
+const Navbar = ({
+  currentUser,
+  manager,
+  serviceCompanyAdmin,
+  serviceCompany,
+}: NavbarProps) => {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const theme = useTheme();
+
+  const handleOpenNavMenu = (event: any) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleOpenUserMenu = (event: any) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  let pages = ["Notifications", "Payments"];
+  let settings = ["Profile", "Account", "Logout"];
+
+  if (!currentUser) {
+    pages = ["About us", "Pricing", "Login"];
+    settings = ["Login", "Products"];
+  }
+
+  if (manager) {
+    pages = ["Announcements", "Users", "Add User", "Payments", "Auctions"];
+    settings = ["Profile", "Account", "Logout"];
+  }
+
+  if (serviceCompanyAdmin) {
+    pages = ["Companies", "Add Company"];
+    settings = ["Profile", "Account", "Logout"];
+  }
+
+  if (serviceCompany) {
+    pages = ["Auctions", "Requests"];
+    settings = ["Profile", "Account", "Logout"];
+  }
+
+  return (
+    <AppBar position="fixed">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component={StyledLink}
+            to="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+            }}
+          >
+            BLANKFACTOR
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: {
+                xs: "block",
+                md: "none",
+              },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              sx={{
+                color:
+                  theme.palette.mode === "dark"
+                    ? theme.palette.primary.main
+                    : theme.palette.grey[50],
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              PaperProps={{
+                style: {
+                  zIndex: 1300,
+                },
+              }}
+              sx={{
+                color:
+                  theme.palette.mode === "dark"
+                    ? theme.palette.grey[500]
+                    : theme.palette.grey[50],
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <StyledDropdownLink
+                    to={`/${page.replace(/\s/g, "").toLowerCase()}`}
+                  >
+                    {page}
+                  </StyledDropdownLink>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h5"
+            noWrap
+            component={StyledLink}
+            to=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+            }}
+          >
+            BLANKFACTOR
+          </Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "flex-end",
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  my: 2,
+                  mx: 2,
+                  display: "block",
+                  transition: "transform 0.3s",
+                  "&:hover": {
+                    transform: "translateY(-3px)",
+                  },
+                }}
+              >
+                <StyledLink to={`/${page.replace(/\s/g, "").toLowerCase()}`}>
+                  {page}
+                </StyledLink>
+              </Button>
+            ))}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar
+                  alt="Remy Sharp"
+                  src="https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //just for testing
+                />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">
+                    <StyledDropdownLink to={`/${setting.toLowerCase()}`}>
+                      {setting}
+                    </StyledDropdownLink>
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default Navbar;
