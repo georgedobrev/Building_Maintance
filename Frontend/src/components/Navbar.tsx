acc6200d78dbec20fbb71bf2347be5c67b879cfe
AntonioNik <131446929+AntonioNik@users.noreply.github.com>
2023-06-02 09:25:08 +0300

    Update imports in Navbar.tsx

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
 import { styled, useTheme } from "@mui/system";
-import NavbarProps from "./NavbarProps";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Avatar,
+  Button,
+  Tooltip,
+  MenuItem,
+} from "@mui/material";
 
 const StyledLink = styled(Link)(({ theme }) => ({
   textDecoration: "none",
