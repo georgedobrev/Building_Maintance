49c56fba65c7cc3fb6348636beb625f00a65fed7
AntonioNik <131446929+AntonioNik@users.noreply.github.com>
2023-06-09 18:10:46 +0300

    Ordered the imports in main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "../src/store/DarkMode/store";
-import App from "./App.tsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./main.css";
+import { store } from "../src/store/DarkMode/store";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_ID}>
