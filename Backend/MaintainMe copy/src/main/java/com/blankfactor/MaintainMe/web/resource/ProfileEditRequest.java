99b59289910aaa3dc97ebf8932807c7cc5933536
prodanov33 <robert.prodanov@blankfactor.com>
2023-06-21 21:41:32 +0300

    .

---
+++ Backend/MaintainMe copy/src/main/java/com/blankfactor/MaintainMe/web/resource/ProfileEditRequest.java
@@ -0,0 +1,35 @@
+package com.blankfactor.MaintainMe.web.resource;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileEditRequest {
+
+    @NotBlank
+    @Size(min = 6,max = 20)
+    private String username;
+    @NotBlank
+    @Size(min = 8,max = 50)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$")
+    private String password;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Size(min = 2,max = 32)
+    private String firstName;
+    @NotBlank
+    @Size(min = 2,max = 32)
+    private String lastName;
+
+}
