9c0f8baf771d365a81ba7cc7c28905140818449c
RobertProdanov03 <robert.prodanov@blankfactor.com>
2023-06-28 10:56:35 +0300

    fixed send email method

--- Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/service/EmailService.java
+++ Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/service/EmailService.java
@@ -1,12 +1,18 @@
 package com.blankfactor.MaintainMe.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class EmailService {
     private final JavaMailSender emailSender;
+
 
     public EmailService(JavaMailSender emailSender) {
         this.emailSender = emailSender;
@@ -17,6 +23,31 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+        emailSender.send(message);
+    }
+
+        public void sendEmailForgottenPassword(String recipientEmail, String link)
+            throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+        helper.setFrom("contact@shopme.com", "MaintainMe Support");
+        helper.setTo(recipientEmail);
+
+        String subject = "Here's the link to reset your password";
+
+        String content = "<p>Hello,</p>"
+                + "<p>You have requested to reset your password.</p>"
+                + "<p>Click the link below to change your password:</p>"
+                + "<p><a href=\"" + link + "\">Change my password</a></p>"
+                + "<br>"
+                + "<p>Ignore this email if you do remember your password, "
+                + "or you have not made the request.</p>";
+
+        helper.setSubject(subject);
+
+        helper.setText(content, true);
+
         emailSender.send(message);
     }
 }
