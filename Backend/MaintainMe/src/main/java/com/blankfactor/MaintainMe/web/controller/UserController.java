5ee64b18c8c9cc6b7a809a37e1013e5e142d9651
martinbardarov-exe <martin.bardarov@blankfactor.com>
2023-06-28 10:31:13 +0300

    fix

--- Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/web/controller/UserController.java
+++ Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/web/controller/UserController.java
@@ -37,8 +37,6 @@ public class UserController {
         }
     }
 
-    //TODO try and catch block for already existing user
-
     @PostMapping("/manage/register/user")
     public ResponseEntity<String> createManagerUser(@Valid @RequestBody ManagerCreateUser managerCreateUser)
             throws UserAlreadyExistsException {
@@ -47,7 +45,7 @@ public class UserController {
             return ResponseEntity.ok("User created successfully by the manager");
         } catch (UserAlreadyExistsException e) {
             // Handle the exception and return an appropriate response
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("User already exists");
+            return ResponseEntity.status(HttpStatus.OK).body("User already exists");
         }
     }
 
