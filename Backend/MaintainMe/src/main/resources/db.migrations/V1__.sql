0b7b5a5a65df3d8be95f2f3c5714096ad20a9d3e
martinbardarov-exe <martin.bardarov@blankfactor.com>
2023-06-29 15:02:16 +0300

    .

--- Backend/MaintainMe/src/main/resources/db.migrations/V1__.sql
+++ Backend/MaintainMe/src/main/resources/db.migrations/V1__.sql
@@ -95,7 +95,7 @@ CREATE TABLE user
     email      VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
     last_name  VARCHAR(255) NOT NULL,
-    password   VARCHAR(255) ,
+    password   VARCHAR(255) NOT NULL,
     CONSTRAINT PK_USER PRIMARY KEY (id),
     UNIQUE (email),
 );
