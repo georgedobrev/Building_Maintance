d6007677fec6faec735fb835429b5945d45fa5d6
AntonioNik <131446929+AntonioNik@users.noreply.github.com>
2023-07-10 13:29:51 +0300

    Bug/b41 change the object for registering a user (#36)
    * the navbar is no longer visible while in login page
    
    * extracting userId from BE response and storing in localstorage for later usage
    
    * cHANGED THE STRUCTURE OF REG response
    
    * test

--- Frontend/src/config/config.ts
+++ Frontend/src/config/config.ts
@@ -5,4 +5,8 @@ export const config = {
   login: "auth/login",
   api_all_countries: "https://restcountries.com/v3.1/all",
   get_user_role: "user/role",
+  register_user: "auth/manager/register/user",
+  get_managed_buildings: (id: string | number) => `building/${id}`,
+  get_building_units: (buildingId: string | number) =>
+    `units/managed/buildings/${buildingId}/units`,
 };
