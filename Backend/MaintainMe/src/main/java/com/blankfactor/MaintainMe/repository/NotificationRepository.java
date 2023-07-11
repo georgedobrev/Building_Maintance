1f86cb2c41302a0039cb6a5034ae5243a33545a5
RobertProdanov03 <robert.prodanov@blankfactor.com>
2023-07-11 15:02:20 +0300

    updated delete methodd

--- Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/repository/NotificationRepository.java
+++ Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/repository/NotificationRepository.java
@@ -1,7 +1,10 @@
 package com.blankfactor.MaintainMe.repository;
 
+import com.blankfactor.MaintainMe.entity.Invoice;
 import com.blankfactor.MaintainMe.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +13,9 @@ import java.util.List;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> getNotificationByBuildingId(Long buildingId);
+
+    @Query(value = "SELECT * FROM notification u WHERE u.id = :id", nativeQuery = true)
+    Notification getNotificationById(@Param("id") Long id);
 
 
 }
