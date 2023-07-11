22f0ba588fc65cc097333901bed96020946813e7
RobertProdanov03 <robert.prodanov@blankfactor.com>
2023-07-11 09:56:57 +0300

    added usage of jwt token

--- Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/service/NotificationService.java
+++ Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/service/NotificationService.java
@@ -4,22 +4,20 @@ import com.blankfactor.MaintainMe.entity.Building;
 import com.blankfactor.MaintainMe.entity.Notification;
 import com.blankfactor.MaintainMe.entity.User;
 import com.blankfactor.MaintainMe.repository.BuildingRepository;
+import com.blankfactor.MaintainMe.repository.LocalUserRepository;
 import com.blankfactor.MaintainMe.repository.NotificationRepository;
 import com.blankfactor.MaintainMe.repository.UserRoleBuildingRepository;
+
 import com.blankfactor.MaintainMe.web.exception.InvalidNotificationException;
 import com.blankfactor.MaintainMe.web.resource.Notification.NotificationByBuildingRequest;
 import com.blankfactor.MaintainMe.web.resource.Notification.NotificationDeleteRequest;
 import com.blankfactor.MaintainMe.web.resource.Notification.NotificationEditRequest;
 import com.blankfactor.MaintainMe.web.resource.Notification.NotificationRequest;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import java.util.Date;
 import java.util.List;
-
 @Service
 @AllArgsConstructor
 public class NotificationService {
@@ -28,44 +26,43 @@ public class NotificationService {
     private final BuildingRepository buildingRepository;
     private final UserRoleBuildingRepository userRoleBuildingRepository;
     private final EmailService emailService;
+    private final JWTService jwtService;
+    private final LocalUserRepository userRepository;
 
-    public List<Notification> getAllNotificationsByBuilding(NotificationByBuildingRequest request){
+
+    public List<Notification> getAllNotificationsByBuilding(NotificationByBuildingRequest request) {
         return notificationRepository.getNotificationByBuildingId(request.getId());
     }
-
 
     public Notification sendNotification(NotificationRequest notificationRequest) throws Exception {
 
         Building building =  buildingRepository.findById(notificationRequest.getBuildingId())
                 .orElseThrow(() -> new Exception("Building not found"));
-
-        User authUser = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-
+        String email =   jwtService.getEmail(notificationRequest.getToken());
+        User authUser = userRepository.getUserByEmail(email);
         Date date = new Date();
-
         List<String> emailsToSend = userRoleBuildingRepository.getUsersByBuildingId(building.getId());
 
         try {
+
             var notification = Notification.builder()
-                    .messageTitle(notificationRequest.getMessageTitle())
-                    .information(notificationRequest.getInformation())
+                    .messageTitle(notificationRequest.getTitle())
+                    .information(notificationRequest.getDescription())
                     .date(date)
                     .building(building)
                     .user(authUser)
                     .build();
 
             for (int i =0; i <emailsToSend.size();i++){
-                emailService.sendEmail(emailsToSend.get(i), notificationRequest.getMessageTitle(), notificationRequest.getInformation());
+                emailService.sendEmail(emailsToSend.get(i), notificationRequest.getTitle(), notificationRequest.getDescription());
             }
 
             notificationRepository.save(notification);
-
         }catch (Exception ex){
             throw new InvalidNotificationException(ex.getMessage());
         }
         return null;
     }
-
     @Transactional(rollbackFor = Exception.class)
     public Notification editNotification(NotificationEditRequest notificationEditRequest) throws Exception {
 
@@ -78,14 +75,11 @@ public class NotificationService {
         notification.setInformation(notificationEditRequest.getInformation());
         notification.setMessageTitle(notificationEditRequest.getMessageTitle());
         notification.setBuilding(building);
-
         notificationRepository.save(notification);
-
         return null;
     }
-
     public Notification deleteNotification(NotificationDeleteRequest notificationDeleteRequest) {
-
+        
         try {
             notificationRepository.deleteById(notificationDeleteRequest.getId());
         }catch (Exception ex){
--- Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/web/resource/Notification/NotificationRequest.java
+++ Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/web/resource/Notification/NotificationRequest.java
@@ -1,26 +1,20 @@
 package com.blankfactor.MaintainMe.web.resource.Notification;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
-
-import java.util.Date;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class NotificationRequest {
 
-
+        private String token;
         @NotBlank(message = "message title cannot be empty.")
         @NotEmpty
-        private String messageTitle;
-
+        private String title;
         @NotBlank(message = "message information cannot be empty.")
         @NotEmpty
-        private String information;
-
+        private String description;
         private Long buildingId;
 
 }
