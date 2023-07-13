d03b334532505d93251e8c2603144021324394ec
RobertProdanov03 <robert.prodanov@blankfactor.com>
2023-07-13 15:38:32 +0300

    fix1

--- Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/web/controller/GoogleController.java
+++ Backend/MaintainMe/src/main/java/com/blankfactor/MaintainMe/web/controller/GoogleController.java
@@ -1,9 +1,11 @@
 package com.blankfactor.MaintainMe.web.controller;
 
+import com.blankfactor.MaintainMe.entity.Comment;
 import com.blankfactor.MaintainMe.entity.User;
 import com.blankfactor.MaintainMe.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,8 +20,8 @@ public class GoogleController {
   private final UserService userService;
 
     @GetMapping()
-    public User getCurrentUser(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
-        return userService.getCurrentUser(oAuth2AuthenticationToken);
+    public ResponseEntity<User> getCurrentUser(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+        return ResponseEntity.ok( userService.getCurrentUser(oAuth2AuthenticationToken));
     }
 
 }
